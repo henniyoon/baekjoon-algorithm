bfe574829656911ffe0c9a623a86bd3d346a0159
Hennie <54562505+henniyoon@users.noreply.github.com>
2021-04-12 00:12:01 +0900

    03. for문
    - 2742번 : 기찍 N

--- src/baekjoon_ch03/Main06.java
+++ src/baekjoon_ch03/Main06.java
@@ -1,16 +1,17 @@
 package baekjoon_ch03;
-// 2741 : N 찍기
+// 2742 : 기찍 N
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-public class Main05 {
+public class Main06 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int n = Integer.parseInt(br.readLine());
-		for(int i=1; i<=n; i++) {
+		
+		for(int i=n; i>=1; i--) {
 			bw.write(i + "\n");
 		}
 		br.close();
