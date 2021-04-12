e5e2a3bb498dbcfef43f42fb8587942ba25fbe7d
Hennie <54562505+henniyoon@users.noreply.github.com>
2021-04-12 21:03:56 +0900

    03. for문
    - 11021번 : A+B - 7

---
+++ src/baekjoon_ch03/Main07.java
@@ -0,0 +1,31 @@
+package baekjoon_ch03;
+// 11021 : A+B - 7
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main07 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		int t = Integer.parseInt(br.readLine());
+		int arr[][] = new int[t][2];
+		int sum[] = new int[t];
+		for(int i=0; i<t; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j=0; j<2; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		for(int i=0; i<t; i++) {
+			sum[i] = arr[i][0] + arr[i][1];
+			bw.write("Case #" + (i+1) + ": " + sum[i] + "\n");
+		}
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+}
