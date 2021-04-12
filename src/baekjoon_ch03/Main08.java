95b76f8359aa4a773de6067158c81a5d4f0e255c
Hennie <54562505+henniyoon@users.noreply.github.com>
2021-04-12 21:04:23 +0900

    03. for문
    - 11022번 : A+B - 8

---
+++ src/baekjoon_ch03/Main08.java
@@ -0,0 +1,32 @@
+package baekjoon_ch03;
+// 11022 : A+B - 8
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main08 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		int t = Integer.parseInt(br.readLine());
+		int arr[][] = new int[t][2];
+		int sum[] = new int[t];
+		for(int i=0; i<t; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j=0; j<2; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		for(int i=0; i<t; i++) {
+			sum[i] = arr[i][0] + arr[i][1];
+			bw.write("Case #" + (i+1) + ": " 
+			+ arr[i][0] + " + " + arr[i][1] + " = " + sum[i] + "\n");
+		}
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+}
