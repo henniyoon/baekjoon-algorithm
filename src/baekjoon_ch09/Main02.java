273dccbed0783a3c51ac192cf20f39a030a5dadd
Hennie <2749126@naver.com>
2021-06-15 23:47:50 +0900

    09. 기본 수학 2
    - 2581번 : 소수 (왜 안돼..)

--- src/baekjoon_ch09/Main02.java
+++ src/baekjoon_ch09/Main02.java
@@ -1,57 +1,55 @@
 package baekjoon_ch09;
-//2581 : 소수
+// 2581 : 소수
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main02 {
 	public static boolean[] prime;	// 소수를 체크할 배열
+	
 	public static void main(String[] args) throws Exception {
- 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		
-		br.readLine();
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int count = 0;
+		int M = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(br.readLine());
 		
-		while(st.hasMoreTokens()) {
-			int num = Integer.parseInt(st.nextToken());
-			
-			make_prime(1000);
-			 
-			if(prime[num] == false) {	// 소수(false)일 경우 count++
-				count++;
+		int min = 0;	// 최솟값
+		int sum = 0;	// 소수의 합
+		
+		make_prime(N);
+		for(int i=M; i<=N; i++) {
+			if(prime[i] == false) {	// 소수이면
+				min = i;
+				break;
 			}
 		}
-		System.out.println(count);
+		for(int i=M; i<=N; i++) {
+			if(prime[i] == false) {
+				sum += i;
+			}
+		}
+		if(min == 0) {	// 범위 내에 소수가 없으면
+			sb.append(-1);
+		} else {		// 범위 내에 소수가 있으면
+			sb.append(sum).append("\n").append(min).append("\n");
+		}
+		System.out.println(sb);
 	}
  
-	// N 이하 소수 생성 메소드 
-	public static void make_prime(int N) {
-		
-		prime = new boolean[N + 1];	// 0 ~ N
+	public static void make_prime(int n) {
+		// 소수가 아니면 true 소수이면 false
+		prime = new boolean[n + 1];	// 0 ~ n
  
-		/*
-		소수가 아닌 index = true
-		소수인 index = false
-		*/
-		
-		// 2 미만의 N 을 입력받으면 소수는 판별할 필요 없으므로 바로 return
-		if(N < 2) {
+		if(n < 2) {	// n이 2 미만이면 만들 소수가 없으므로 바로 return
 			return;
 		}
-        
 		prime[0] = prime[1] = true;
-		
-        
 		// 제곱근 함수 : Math.sqrt()
-		for(int i = 2; i <= Math.sqrt(N); i++) {
-        
+		for(int i = 2; i <= Math.sqrt(n); i++) {
 			// 이미 체크된 배열이면 다음 반복문으로 skip
-			if(prime[i]==true) {
+			if(prime[i] == true) {
 				continue;
 			}
-        
 			// i 의 배수들을 걸러주기 위한 반복문
 			for(int j = i*i; j < prime.length; j = j+i) {
 				prime[j] = true;
